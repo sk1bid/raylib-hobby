f2196c90593658c177987abb79a726b374642a5e
sk1bid <ivanov.artem.bld@gmail.com>
2025-07-13 10:36:02 +0700

    Realise DrawTextCentered

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,4 @@
-#include "utils.h"
+#include "my_utils.h"
 #include <stdbool.h>
 #include "raylib.h"
 
@@ -16,4 +16,10 @@ bool TimerDone(Timer timer)
 double GetElapsed(Timer timer)
 {
     return GetTime() - timer.startTime;
+}
+
+void DrawTextCentered(const char* text, int fontSize, int posY, Color color){
+    int textWidth = MeasureText(text, fontSize);
+    int posX = (GetScreenWidth() / 2) - (textWidth / 2);
+    DrawText(text, posX, posY, fontSize, color);
 }
