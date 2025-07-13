690cca97de95c4826f26d5d2a5a04bc95774400e
sk1bid <ivanov.artem.bld@gmail.com>
2025-07-13 14:30:43 +0700

    Realise button navigation in menu

--- src/screen_title.c
+++ src/screen_title.c
@@ -26,6 +26,10 @@
 
 #include "raylib.h"
 #include "screens.h"
+#include "my_utils.h"
+
+#define NUM_FRAMES 4
+#define MAX_BUTTONS 2
 
 //----------------------------------------------------------------------------------
 // Module Variables Definition (local)
@@ -33,6 +37,24 @@
 static int framesCounter = 0;
 static int finishScreen = 0;
 
+static float frameWidth = 0.0f;
+static float frameHeight = 0.0f;
+static float spriteScale = 0.0f;
+
+static float startButtonPosY = 0;
+static Rectangle startBtnSourceRec = {0};
+static Rectangle startBtnBounds = {0};
+
+static float exitButtonPosY = 0;
+static Rectangle exitBtnSourceRec = {0};
+static Rectangle exitBtnBounds = {0};
+
+static Button buttons[MAX_BUTTONS] = {0};
+
+static Vector2 mousePoint = {0};
+
+static int currentBtn = 0; // number of button
+ 
 //----------------------------------------------------------------------------------
 // Title Screen Functions Definition
 //----------------------------------------------------------------------------------
@@ -40,32 +62,159 @@ static int finishScreen = 0;
 // Title Screen Initialization logic
 void InitTitleScreen(void)
 {
+    ShowCursor();
+    spriteScale = 2.0f;
+    frameHeight = 64.0f;
+    frameWidth = 64.0f;
     // TODO: Initialize TITLE screen variables here!
     framesCounter = 0;
     finishScreen = 0;
+
+    startButtonPosY = 3.0f * frameHeight; // go to the 4 row in sprite
+    startBtnSourceRec = (Rectangle){64 * 4, startButtonPosY + 15, frameWidth, frameHeight - 40}; // start point for our Frame
+    startBtnBounds = (Rectangle){ GetScreenWidth() / 2.0f - frameWidth, GetScreenHeight() / 2.0f - frameHeight/ 2.0f, frameWidth * 2, (frameHeight / 2.5f) * 2};
+
+    exitButtonPosY = 4.0f * frameHeight; // go to the 5 row in sprite
+    exitBtnSourceRec = (Rectangle){64 * 4, exitButtonPosY + 15, frameWidth, frameHeight - 40}; // start point for our Frame
+    exitBtnBounds = (Rectangle){ GetScreenWidth() / 2.0f - frameWidth, GetScreenHeight() / 2.0f + frameHeight, frameWidth * 2, (frameHeight / 2.5f) * 2};
+
+    for (int i = 0; i < MAX_BUTTONS; i++){
+        buttons[i].action = false;
+        buttons[i].state = 0;
+    }
+    
+
+    mousePoint = (Vector2){0.0f, 0.0f};
+
+    currentBtn = -1;
+
+
 }
 
 // Title Screen Update logic
 void UpdateTitleScreen(void)
 {
-    // TODO: Update TITLE screen variables here!
+    mousePoint = GetMousePosition();
+    for (int i = 0; i < MAX_BUTTONS; i++){
+        buttons[i].action = false;
+    }
+    
+    
 
-    // Press enter or tap to change to GAMEPLAY screen
-    if (IsKeyPressed(KEY_ENTER)){
-        finishScreen = 2;
-        PlaySound(fxCoin);
+    if (IsKeyDown(KEY_ENTER)){
+        if (currentBtn == 0){
+            finishScreen = 2; // play game
+            buttons[0].action = true;
+            buttons[0].state = 2;
+        }
+        else if (currentBtn == 1){
+           finishScreen = 1; // close game
+           buttons[0].action = true;
+           buttons[0].state = 2;
+        }
     }
-    if (IsKeyPressed(KEY_O)){
-        finishScreen = 1;
+
+    
+
+    if (CheckCollisionPointRec(mousePoint, startBtnBounds)){
+        currentBtn = -1;
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+            buttons[0].state = 2;
+            
+        }
+        else{
+            buttons[0].state = 1;
+        }
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+            buttons[0].action = true;
+            finishScreen = 2;
+        }
+        TraceLog(LOG_INFO, "MOUSE: %d", currentBtn); 
     }
+    else if (CheckCollisionPointRec(mousePoint, exitBtnBounds)){
+        currentBtn = -1;
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+            buttons[1].state = 2;
+            
+        }
+        else{
+            buttons[1].state = 1;
+        }
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+            buttons[1].action = true;
+            finishScreen = 2;
+        }
+        TraceLog(LOG_INFO, "MOUSE: %d", currentBtn); 
+    }
+
+    else if (IsKeyPressed(KEY_DOWN)){
+        for (int i =0; i < MAX_BUTTONS; i++){
+            buttons[i].state = 0;
+        }
+        currentBtn++;
+        if (currentBtn > MAX_BUTTONS - 1){
+            currentBtn = 0;
+        }
+        buttons[currentBtn].state = 1;
+        TraceLog(LOG_INFO, "KEY DOWN: %d", currentBtn);
+    }  
+    else if (IsKeyPressed(KEY_UP)){
+        for (int i =0; i < MAX_BUTTONS; i++){
+            buttons[i].state = 0;
+        }
+        currentBtn--;
+        if (currentBtn < 0){
+            currentBtn = MAX_BUTTONS - 1;
+        }
+        buttons[currentBtn].state = 1;
+        TraceLog(LOG_INFO, "KEY_UP: %d", currentBtn);
+        
+    }
+    else if (currentBtn == -1){
+        for (int i =0; i < MAX_BUTTONS; i++){
+            buttons[i].state = 0;
+    }
+    }
+    
+    
+
+    for (int i = 0; i < MAX_BUTTONS; i++){
+        if (buttons[i].action){
+            PlaySound(fxCoin);
+        }
+    }
+
+    startBtnSourceRec.x = buttons[0].state * frameWidth + frameWidth * 4;
+    startBtnSourceRec.y = startButtonPosY + 15;
+    
+    exitBtnSourceRec.x = buttons[1].state * frameWidth + frameWidth * 4;
+    exitBtnSourceRec.y = exitButtonPosY + 15;
 }
 
 // Title Screen Draw logic
 void DrawTitleScreen(void)
 {
     // TODO: Draw TITLE screen here!
-    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), GREEN);
-    DrawText("PRESS ENTER TO START GAME ('O' TO GO SETTINGS)", 120, 220, 20, DARKGREEN);
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(LIME, 0.5));
+    DrawTextCentered("DOODLE", 70, 200, BLACK);
+    DrawTexturePro(
+        pixelButtonsTexture, 
+        startBtnSourceRec, 
+        startBtnBounds,
+        (Vector2){0, 0}, 
+        0.0f,            
+        WHITE            
+    );
+    DrawTexturePro(
+        pixelButtonsTexture, 
+        exitBtnSourceRec, 
+        exitBtnBounds,
+        (Vector2){0, 0}, 
+        0.0f,            
+        WHITE            
+    );
+    DrawRectangleLinesEx(startBtnBounds, 2, RED);
+    DrawRectangleLinesEx(exitBtnBounds, 2, RED);
 }
 
 // Title Screen Unload logic
