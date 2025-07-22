f1e10aaf71bd59d82bfa7b82d5f994c4b49e4079
sk1bid <ivanov.artem.bld@gmail.com>
2025-07-22 09:01:38 +0700

    Move constants to the header

--- include/gameplay.h
+++ include/gameplay.h
@@ -1,8 +1,16 @@
 #ifndef GAMEPLAY_H
 #define GAMEPLAY_H
 
-#include "raylib.h"
+#define G 1000.0f
+#define PLAYER_JUMP_SPEED 700.0f
+#define PLAYER_HOR_SPEED 250.0f
+#define MAX_PLATFORMS 100
 
+#define PLATFORM_WIDTH 80
+#define PLATFORM_HEIGHT 20
+
+#include "raylib.h"
+#include "raymath.h"
 
 typedef struct Player {
     Rectangle collaider;
